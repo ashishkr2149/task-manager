2d58851c0c7b2552ccb0a3c2bb66a279ef6c2c6d
Ashish <ashishkr.2149@gmail.com>
2024-07-19 15:37:21 +0530

    Adding morgan

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
+import morgan from "morgan";
 
 //Configure Env
 dotenv.config();
 
-// Configure DB
+//Configure DB
 connectDB();
 
 // REST Object
 const app = express();
+
+//Custom fromat for Morgan
+const customFormat = ":method :url :status :response-time ms";
+
+//Middlewares
+app.use(express.json());
+app.use(morgan(customFormat));
 
 //REST API
 app.get("/", (req, res) => {
